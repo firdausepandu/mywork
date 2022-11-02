16b4ce9b552bf5d1f2d5d13d0e73677cb89043a1
firdausepandu <firdaus@epandu.com>
2022-11-02 18:01:11 +0800

    modify password field and login button

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:share_plus/share_plus.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,30 +14,85 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         title: 'MyWork',
         theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
-          primarySwatch: Colors.blue,
-          scaffoldBackgroundColor: Color(0xff29347d)
-        ),
+            // This is the theme of your application.
+            //
+            // Try running your application with "flutter run". You'll see the
+            // application has a blue toolbar. Then, without quitting the app, try
+            // changing the primarySwatch below to Colors.green and then invoke
+            // "hot reload" (press "r" in the console where you ran "flutter run",
+            // or simply save your changes to "hot reload" in a Flutter IDE).
+            // Notice that the counter didn't reset back to zero; the application
+            // is not restarted.
+            primarySwatch: Colors.blue,
+            scaffoldBackgroundColor: const Color(0xff29347d)),
         home: Scaffold(
             body: Center(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(padding: const EdgeInsets.fromLTRB(20, 20, 20, 70), child: Image.asset("asset/img/MyWork_Employee.png",height: 100,width: 100,),),
-                  Container(padding: const EdgeInsets.fromLTRB(20, 20, 20, 0), child: TextField(decoration: InputDecoration(prefixIcon:Icon(Icons.person,color:Colors.cyanAccent),border: OutlineInputBorder(borderRadius: BorderRadius.circular(90.0),), labelText: 'USERNAME',labelStyle: TextStyle(color: Colors.grey)),),),
-                  Container(padding: const EdgeInsets.fromLTRB(20, 20, 20, 0), child: TextField(obscureText: true, decoration: InputDecoration(prefixIcon:Icon(Icons.lock,color:Colors.cyanAccent),border: OutlineInputBorder(borderRadius: BorderRadius.circular(90.0),borderSide: BorderSide(width: 5)), labelText: 'PASSWORD',labelStyle: TextStyle(color: Colors.grey)),),),
-                  Container(height: 80, padding: const EdgeInsets.all(20), child: ElevatedButton(style: ElevatedButton.styleFrom(minimumSize: const Size.fromHeight(50),primary: Colors.white), child: const Text('SIGN IN',style: TextStyle(color: Color(0xff29347d)),), onPressed: () {},)),
-                ],
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                padding: const EdgeInsets.fromLTRB(20, 20, 20, 70),
+                child: Image.asset(
+                  "asset/img/MyWork_Employee.png",
+                  height: 100,
+                  width: 100,
+                ),
               ),
-            )));
+              Container(
+                padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+                child: TextField(
+                  decoration: InputDecoration(
+                      prefixIcon:
+                          const Icon(Icons.person, color: Colors.cyanAccent),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(90.0),
+                      ),
+                      labelText: 'USERNAME',
+                      labelStyle: TextStyle(color: Colors.grey)),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: '*****',
+                    prefixIcon: Icon(Icons.lock, color: Colors.cyanAccent),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(90.0),
+                      borderSide: BorderSide(width: 5),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red, width: 0.0),
+                      borderRadius: BorderRadius.circular(90.0),
+                    ),
+                    labelText: 'PASSWORD',
+                    labelStyle: TextStyle(
+                      color: Colors.grey,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                  height: 80,
+                  padding: const EdgeInsets.all(20),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: const Size.fromHeight(50),
+                      backgroundColor: Colors.white,
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                      ),
+                    ),
+                    child: const Text(
+                      'SIGN IN',
+                      style: TextStyle(color: Color(0xff29347d)),
+                    ),
+                    onPressed: () {},
+                  )),
+            ],
+          ),
+        )));
   }
 }
