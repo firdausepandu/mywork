7674003a5833d9c1665d8d60f734cb864d58ffa8
firdausepandu <firdaus@epandu.com>
2022-11-03 16:13:25 +0800

    add appbar with menu, central text, action

--- lib/mainmenu.dart
+++ lib/mainmenu.dart
@@ -10,9 +10,34 @@ class MainMenu extends StatelessWidget {
       title: "Main Menu",
       theme: ThemeData(
           fontFamily: GoogleFonts.raleway().fontFamily,
-          primarySwatch: Colors.blue,
-          scaffoldBackgroundColor: const Color(0xff29347d)),
-      home: Scaffold(),
+          primarySwatch: Colors.cyan,
+          scaffoldBackgroundColor: Colors.white),
+      home: Scaffold(
+        appBar:AppBar(
+          title: const Text("MyWork - 012 594 2896",style: TextStyle(color: Colors.white)),
+          centerTitle: true,
+          leading: GestureDetector(
+            onTap: () { /* Write listener code here */ },
+            child: const CircleAvatar(
+              radius: 30,
+              backgroundColor: Color(0xff29347d),
+              child: Icon(Icons.menu)
+            ),
+          ),
+          actions: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(right:20.0),
+              child: GestureDetector(
+                onTap: (){},
+                child: const Icon(Icons.settings_outlined,color:Colors.white),
+              ),
+            ),
+
+          ],
+        ),
+        body: const Center()
+
+      ),
     );
   }
 }
