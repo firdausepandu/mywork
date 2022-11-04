2ba6037aa7a8eec9c990fb0fd8693e0e0b76a78c
firdausepandu <firdaus@epandu.com>
2022-11-04 10:42:45 +0800

    modify container padding to centre the children

--- lib/mainmenu.dart
+++ lib/mainmenu.dart
@@ -13,30 +13,88 @@ class MainMenu extends StatelessWidget {
           primarySwatch: Colors.cyan,
           scaffoldBackgroundColor: Colors.white),
       home: Scaffold(
-        appBar:AppBar(
-          title: const Text("MyWork - 012 594 2896",style: TextStyle(color: Colors.white)),
+        appBar: AppBar(
+          title: const Text("MyWork - 012 594 2896",
+              style: TextStyle(color: Colors.white)),
           centerTitle: true,
           leading: GestureDetector(
-            onTap: () { /* Write listener code here */ },
-            child: const CircleAvatar(
-              radius: 30,
-              backgroundColor: Color(0xff29347d),
-              child: Icon(Icons.menu)
+            onTap: () {/* Write listener code here */},
+            child: const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: CircleAvatar(
+                radius: 30,
+                backgroundColor: Color(0xff29347d),
+                child: Icon(Icons.menu),
+              ),
             ),
           ),
           actions: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(right:20.0),
-              child: GestureDetector(
-                onTap: (){},
-                child: const Icon(Icons.settings_outlined,color:Colors.white),
-              ),
+            // Padding(
+            //   padding: const EdgeInsets.only(right: 20.0),
+            //   child: GestureDetector(
+            //     onTap: () {},
+            //     child: const Icon(Icons.settings_outlined, color: Colors.white),
+            //   ),
+            // ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.settings_outlined, color: Colors.white),
             ),
-
           ],
         ),
-        body: const Center()
-
+        body: GridView.count(
+          primary: false,
+          padding: const EdgeInsets.all(20),
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+          crossAxisCount: 2,
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.all(8),
+              color: const Color.fromARGB(255, 73, 213, 231),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Company Profile",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.home),
+                    iconSize: 150,
+                  ),
+                  const Text("3")
+                ],
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              color: Colors.teal[200],
+              child: const Text('Heed not the rabble'),
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              color: Colors.teal[300],
+              child: const Text('Sound of screams but the'),
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              color: Colors.teal[400],
+              child: const Text('Who scream'),
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              color: Colors.teal[500],
+              child: const Text('Revolution is coming...'),
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              color: Colors.teal[600],
+              child: const Text('Revolution, they...'),
+            ),
+          ],
+        ),
       ),
     );
   }
