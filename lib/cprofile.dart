d6c02ef9b9d87b339f2f14ee7e39cb2b36ebefac
firdausepandu <firdaus@epandu.com>
2022-11-04 14:46:05 +0800

    add profile icon,text,divider

--- lib/cprofile.dart
+++ lib/cprofile.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:MyWork/menu.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -11,44 +13,63 @@ class CProfile extends StatelessWidget {
         theme: ThemeData(
           fontFamily: GoogleFonts.raleway().fontFamily,
           primarySwatch: Colors.cyan,
-          scaffoldBackgroundColor: Colors.white,
+          scaffoldBackgroundColor: const Color(0xff29347d),
         ),
         home: Scaffold(
-            appBar: AppBar(
-              title: const Text("YOUR PROFILE",
-                  style: TextStyle(color: Colors.white)),
-              centerTitle: true,
-              leading: GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const MainMenu()));
-                },
-                child: const Icon(Icons.arrow_back, color: Colors.white),
-              ),
-              actions: [
+          appBar: AppBar(
+            title: const Text("YOUR PROFILE",
+                style: TextStyle(color: Colors.white)),
+            centerTitle: true,
+            leading: GestureDetector(
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => const MainMenu()));
+              },
+              child: const Icon(Icons.arrow_back, color: Colors.white),
+            ),
+            actions: [
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: ElevatedButton(
+                    onPressed: () {
+                      //TODO go to edit user profile
+                    },
+                    style: ElevatedButton.styleFrom(
+                        shape: const StadiumBorder(),
+                        side: const BorderSide(
+                          width: 2.0,
+                          color: Colors.white,
+                        )),
+                    child: const Text('Edit Profile',
+                        style: TextStyle(color: Colors.white))),
+              )
+            ],
+          ),
+          body: Container(
+            alignment: Alignment.center,
+            width: double.infinity,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const <Widget>[
                 Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: ElevatedButton(
-                      onPressed: () {
-                        //TODO go to edit user profile
-                      },
-                      style: ElevatedButton.styleFrom(
-                          shape: const StadiumBorder(),
-                          side: const BorderSide(
-                            width: 2.0,
-                            color: Colors.white,
-                          )),
-                      child: const Text('Edit Profile',
-                          style: TextStyle(color: Colors.white))),
+                  padding: EdgeInsets.all(20.0),
+                  child: Icon(Icons.account_circle,
+                      size: 100, color: Colors.white),
+                ),
+                Text(
+                  'CREATE ACCOUNT',
+                  style: TextStyle(color: Colors.white, fontSize: 24),
+                ),
+                Divider(
+                  height: 20,
+                  thickness: 5,
+                  indent: 80,
+                  endIndent: 80,
+                  color: Colors.white,
                 )
               ],
             ),
-            body: Column(
-              children: const [
-                Text("test"),
-              ],
-            )));
+          ),
+        ));
   }
 }
